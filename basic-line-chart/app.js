1e2d8b702c57c5a1f8d8207d0e5ac7415e77a427
Hong Liang <lianghongbuaa@gmail.com>
2021-07-16 22:49:46 +0800

    add script to load the data

--- basic-line-chart/app.js
+++ basic-line-chart/app.js
@@ -1,6 +1,9 @@
 const draw = async () => {
   const dataset = await d3.csv("data.csv");
   console.log(dataset);
+
+  const xAccessor = (d) => parseInt(d.data);
+  const yAccessor = (d) => parseInt(d.value);
 };
 
 draw();
